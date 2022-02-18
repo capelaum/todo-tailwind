f5aa2a390500aebf6d7c5a252e16404f531b7651
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-02-18 07:39:08 -0300

    config BG image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 
   theme: {
-    extend: {}
+    extend: {
+      // bg-img-todos
+      backgroundImage: (theme) => ({
+        'img-todos': "url('/images/background.png')"
+      })
+    }
   },
 
   plugins: []
