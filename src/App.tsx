724778a018b93bc445c50a4568d71841e7269de4
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-02-18 08:26:56 -0300

    feat: Form component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import { useState } from 'react'
 import { List } from './components/List/List'
 import { Content } from './components/Template/Content'
 import { Header } from './components/Template/Header'
+import { Form } from './components/Form/Form'
+
+import Todo from './model/Todo'
+
 import { initialTodosList } from './data/mock'
 
 function App() {
   const [todos, setTodos] = useState(initialTodosList)
 
+  function createNewTodo(newTodo: Todo) {
+    setTodos(todos.createTodo(newTodo))
+  }
+
   return (
-    <div className="h-screen flex flex-col bg-gray-300">
-      <Header></Header>
+    <div className="h-screen flex flex-col bg-gray-300 relative">
+      <Header>
+        <Form createNewTodo={createNewTodo} />
+      </Header>
       <Content>
         <List
           todos={todos}
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import Todo from '../../model/Todo'
+
+interface FormProps {
+  createNewTodo: (todo: Todo) => void
+}
+
+export function Form({ createNewTodo }: FormProps) {
+  const [description, setDescription] = useState('')
+
+  function handleCreateNewTodo() {
+    if (description.trim() !== '') {
+      const newTodo = Todo.createActive(Math.random(), description)
+      createNewTodo(newTodo)
+      setDescription('')
+    }
+  }
+
+  return (
+    <div className="flex flex-1 justify-center">
+      <input
+        placeholder="Create new todo"
+        type="text"
+        name="todo"
+        id="todo"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        onKeyUp={(e) => e.key === 'Enter' && handleCreateNewTodo()}
+        className={`
+          w-1/2 px-3 py-2 text-sm
+          border-2 border-purple-300 rounded-lg focus:outline-none
+          focus:ring-2 focus:ring-purple-600
+        `}
+      />
+      <button
+        onClick={handleCreateNewTodo}
+        className="ml-3 bg-purple-600 text-white text-xl px-5 py-2 rounded-lg focus:outline-none "
+      >
+        +
+      </button>
+    </div>
+  )
+}
